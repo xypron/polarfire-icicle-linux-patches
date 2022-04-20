c825e3f5771c74d6e07537117e6c7dbe04076093
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-04-20 15:12:52 +0200

    0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- series.sh
+++ series.sh
@@ -18,7 +18,7 @@ git am ../patches/0001-clk-microchip-mpfs-re-parent-the-configurable-clocks.patc
 git am ../patches/0001-clk-microchip-mpfs-add-RTCREF-clock-control.patch
 git am ../patches/0001-riscv-dts-microchip-reparent-mpfs-clocks.patch
 
-git am ../patches/0001-RISC-V-defconfigs-add-VITESSE_PHY.patch
+git am ../patches/0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
 git am ../patches/0001-riscv-config-enable-the-mailbox-framework.patch
 git am ../patches/0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
 git am ../patches/0001-MAINTAINERS-add-polarfire-rng-pci-and-clock-drivers.patch
