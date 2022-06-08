aeca8ea3da43d057faa75f7919f1dccd89b0373c
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-06-08 14:25:37 +0200

    USB patches
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- series.sh
+++ series.sh
@@ -12,3 +12,7 @@ git am ../patches/0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
 git am ../patches/0001-riscv-config-enable-the-mailbox-framework.patch
 git am ../patches/0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
 git am ../patches/0001-MAINTAINERS-add-polarfire-rng-pci-and-clock-drivers.patch
+
+git am ../patches/0001-usb-musb-Add-support-for-PolarFire-SoC-s-musb-contro.patch
+git am ../patches/0001-MAINTAINERS-add-musb-to-PolarFire-SoC-entry.patch
+# git am ../patches/0001-riscv-dts-microchip-fix-icicle-kit-s-dr_mode.patch
