f246db176cfafac5b47f69dfa0931c79586608fc
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-06-08 15:01:42 +0200

    0001-riscv-dts-microchip-re-add-pdma-to-mpfs-device-tree.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- series.sh
+++ series.sh
@@ -16,3 +16,5 @@ git am ../patches/0001-MAINTAINERS-add-polarfire-rng-pci-and-clock-drivers.patch
 git am ../patches/0001-usb-musb-Add-support-for-PolarFire-SoC-s-musb-contro.patch
 git am ../patches/0001-MAINTAINERS-add-musb-to-PolarFire-SoC-entry.patch
 # git am ../patches/0001-riscv-dts-microchip-fix-icicle-kit-s-dr_mode.patch
+
+git am ../patches/0001-riscv-dts-microchip-re-add-pdma-to-mpfs-device-tree.patch
