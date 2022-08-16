b22f7248e52606bbf64032a2726747a56fcc6452
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-08-16 23:14:22 +0200

    Update to kernel v5.18.17
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- series.sh
+++ series.sh
@@ -4,12 +4,12 @@ set -e
 
 git am --abort || true
 git checkout build || git checkout -b build
-git reset --hard v5.18
+git reset --hard v5.18.17
 
-git am ../patches/0001-hwrng-mpfs-add-polarfire-soc-hwrng-support.patch
-git am ../patches/0001-hwrng-mpfs-Enable-COMPILE_TEST.patch
+git am ../0001-hwrng-mpfs-add-polarfire-soc-hwrng-support.patch
+git am ../0001-hwrng-mpfs-Enable-COMPILE_TEST.patch
 
-git am ../patches/0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
-git am ../patches/0001-riscv-config-enable-the-mailbox-framework.patch
-git am ../patches/0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
-git am ../patches/0001-MAINTAINERS-add-polarfire-rng-pci-and-clock-drivers.patch
+git am ../0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
+git am ../0001-riscv-config-enable-the-mailbox-framework.patch
+git am ../0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
+git am ../0001-MAINTAINERS-add-polarfire-rng-pci-and-clock-drivers.patch
