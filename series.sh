6c570d0972041c515db1290499ebdc2cb7ebb7b6
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-08-17 09:27:25 +0200

    Provide usable v5.19.1 config
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- series.sh
+++ series.sh
@@ -8,18 +8,18 @@ git reset --hard v5.19.1
 
 # Ubuntu's config-5.19.0-1002-generic already has all these settings
 # https://lore.kernel.org/linux-riscv/20220422072533.2582084-2-conor.dooley@microchip.com/
-git am ../patches/0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
+git am ../0001-riscv-select-peripheral-drivers-for-polarfire-soc.patch
 
 # Ubuntu's config-5.19.0-1002-generic already has CONFIG_MAILBOX=y
 # https://lore.kernel.org/linux-riscv/20220422072533.2582084-3-conor.dooley@microchip.com/
-git am ../patches/0001-riscv-config-enable-the-mailbox-framework.patch
+git am ../0001-riscv-config-enable-the-mailbox-framework.patch
 
 # Ubuntu's config-5.19.0-1002-generic already has CONFIG_VITESSE_PHY=m
 # https://lore.kernel.org/linux-riscv/20220422072533.2582084-4-conor.dooley@microchip.com/
-git am ../patches/0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
+git am ../0001-riscv-select-vitesse-phy-driver-for-polarfire-soc.patch
 
 # https://lore.kernel.org/linux-riscv/20220613114642.1615292-2-conor.dooley@microchip.com/
-git am ../patches/0001-usb-musb-Add-support-for-PolarFire-SoC-s-musb-contro.patch
+git am ../0001-usb-musb-Add-support-for-PolarFire-SoC-s-musb-contro.patch
 
 # i2c
 # https://lore.kernel.org/all/20220603071601.1849562-1-conor.dooley@microchip.com/
